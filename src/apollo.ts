2c02bced07f91685fbeed73fdbce356eaf45c600
Rafael Santos <rafaelnsantos1@gmail.com>
2020-01-08 22:45:37 -0300

    format error

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,8 @@
 const { ApolloServer, makeExecutableSchema } = require('apollo-server-express')
+const path = require('path')
 const { exportFolder } = require('../lib/folder-utils')
 const schema = require('./schema')
+const formatError = require('./formatError')
 
 const repositories = exportFolder(process.cwd(), '-repository', { basepath: 'src/repositories' })
 const services = exportFolder(process.cwd(), '-service', { basepath: 'src/services' })
@@ -12,9 +14,11 @@ const utils = exportFolder(process.cwd(), '-util', {
   }
 })
 
+const { CODES } = require(path.join(process.cwd(), 'src/errors'))
+
 module.exports = new ApolloServer({
   schema: makeExecutableSchema(schema),
-  // formatError,
+  formatError: formatError(CODES),
   context: async ({ req, connection }) => connection
     ? ({
       // user: await controllers.auth.verifyTokenSubscription(connection.context.token),
---
+++ src/formatError.ts
@@ -0,0 +1,20 @@
+const { GraphQLError } = require('graphql')
+
+module.exports = function formatError(CODES){
+  function formatError (err) {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        message: CODES[err.message] || CODES.UNKNOWN_ERROR
+      }
+    }
+  
+    if (!CODES[err.message] && err.extensions) console.log(err.path, err.extensions.exception.stacktrace)
+  
+    return {
+      message: CODES[err.message] || err.message,
+      path: err.path
+    }
+  }
+
+  return formatError
+}
