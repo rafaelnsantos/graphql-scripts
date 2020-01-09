33945c1d5af328763ba14f8fe8c75e9791f649b1
Rafael Santos <rafaelnsantos1@gmail.com>
2020-01-09 15:15:28 -0300

    build and start

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,6 @@
 const tsc = require('tsc-prog')
 const path = require('path')
+const fs = require('fs')
 
 tsc.build({
   basePath: process.cwd(), // always required, used for relative paths
@@ -12,4 +13,37 @@ tsc.build({
   },
   include: ['src/**/*'],
   exclude: ['**/*.test.ts', '**/*.spec.ts'],
+})
+
+function recFindByExt (base, ext, files, result) {
+  files = files || fs.readdirSync(base) 
+  result = result || [] 
+
+  files.forEach( 
+      function (file) {
+          var newbase = path.join(base,file)
+          if ( fs.statSync(newbase).isDirectory() )
+          {
+              result = recFindByExt(newbase,ext,fs.readdirSync(newbase),result)
+          }
+          else
+          {
+              if ( file.substr(-1*(ext.length+1)) == '.' + ext )
+              {
+                  result.push(newbase)
+              } 
+          }
+      }
+  )
+  return result
+}
+
+
+const graphqlFiles = recFindByExt(path.join(process.cwd(), 'src/graphql'), 'graphql')
+
+graphqlFiles.map(async file => {
+  console.log('copiando arquivo', file)
+  fs.copyFile(file, file.replace('src', 'build'), err => {
+    if (err) console.log(err)
+  })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV === 'dev') {
   const dotenv = require('dotenv')
   const path = require('path')
   dotenv.config({ path: path.join(process.cwd(), '.env.development') })
+} else {
+  dotenv.config({ path: path.join(__dirname, '..', '.env') })
 }
 
 const app = require('./app')
