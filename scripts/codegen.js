b71d43212408e4dac93204b9b6e25baa5d2a79b0
Rafael Santos <rafaelnsantos1@gmail.com>
2020-01-09 00:20:09 -0300

    codegen

---
+++ scripts/codegen.js
@@ -0,0 +1,27 @@
+const { codegen } = require('@graphql -codegen/core')
+const schema = require('../src/schema')
+const typescriptPlugin = require('@graphql-codegen/typescript')
+const { printSchema, parse } = require('graphql')
+const fs = require('fs')
+
+const outputFile = 'src/generated/schema.ts'
+const config = {
+  // used by a plugin internally, although the 'typescript' plugin currently
+  // returns the string output, rather than writing to a file
+  filename: outputFile,
+  schema: parse(printSchema(schema)), 
+  plugins: [ // Each plugin should be an object
+    {
+      typescript: {}, // Here you can pass configuration to the plugin
+    },
+  ],
+  pluginMap: {
+    typescript: typescriptPlugin,
+  },
+}
+
+const output = await codegen(config)
+
+fs.writeFile(path.join(process.cwd(), outputFile), output, () => {
+  console.log('Outputs generated!')
+})
