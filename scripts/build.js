3da76b6d5ae9834f3ddab67a8af36ba6d4caeb35
Rafael Santos <rafaelnsantos1@gmail.com>
2020-01-10 23:09:32 -0300

    fix build folders and add clean before build

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,16 @@
 const tsc = require('tsc-prog')
 const path = require('path')
 const fs = require('fs')
+const rimraf = require('rimraf')
+
+const buildPath = path.join(process.cwd(), 'build')
+
+rimraf.sync(buildPath)
+
+fs.mkdirSync(buildPath)
+fs.mkdirSync(path.join(buildPath, 'graphql'))
+fs.mkdirSync(path.join(buildPath, 'graphql/_directives'))
+fs.mkdirSync(path.join(buildPath, 'graphql/_scalars'))
 
 tsc.build({
   basePath: process.cwd(), // always required, used for relative paths
@@ -12,29 +22,23 @@ tsc.build({
       skipLibCheck: true,
   },
   include: ['src/**/*'],
-  exclude: ['**/*.test.ts', '**/*.spec.ts'],
+  exclude: ['**/*.test.ts', '**/*.spec.ts', 'src/generated/**.*'],
 })
 
 function recFindByExt (base, ext, files, result) {
   files = files || fs.readdirSync(base) 
   result = result || [] 
 
-  files.forEach( 
-      function (file) {
-          var newbase = path.join(base,file)
-          if ( fs.statSync(newbase).isDirectory() )
-          {
-              result = recFindByExt(newbase,ext,fs.readdirSync(newbase),result)
-          }
-          else
-          {
-              if ( file.substr(-1*(ext.length+1)) == '.' + ext )
-              {
-                  result.push(newbase)
-              } 
-          }
-      }
-  )
+  files.forEach(function (file) {
+    var newbase = path.join(base,file)
+    if (fs.statSync(newbase).isDirectory()){
+      result = recFindByExt(newbase,ext,fs.readdirSync(newbase),result)
+    } else {
+      if (file.substr(-1*(ext.length+1)) == '.' + ext){
+        result.push(newbase)
+      } 
+    }
+  })
   return result
 }
 
@@ -43,6 +47,9 @@ const graphqlFiles = recFindByExt(path.join(process.cwd(), 'src/graphql'), 'grap
 
 graphqlFiles.map(async file => {
   console.log('copiando arquivo', file)
+  let folder = file.replace('src', 'build')
+  folder = folder.substring(0, folder.lastIndexOf("/") )
+  fs.mkdirSync(folder, { recursive: true })
   fs.copyFile(file, file.replace('src', 'build'), err => {
     if (err) console.log(err)
   })
