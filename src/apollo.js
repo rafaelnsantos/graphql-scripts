0242460a4e1c3457bfe11d6aabd58383495c32d5
Rafael Santos <rafaelnsantos1@gmail.com>
2020-01-11 18:48:46 -0300

    fix

--- src/apollo.js
+++ src/apollo.js
@@ -1,4 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production'
+const mode = isProduction ? 'js' : 'ts'
 
 const { ApolloServer, makeExecutableSchema } = require('apollo-server-express')
 const path = require('path')
@@ -6,17 +7,19 @@ const { exportFolder } = require('folder-utils')
 const schema = require('./schema')(isProduction ? 'js' : 'ts')
 const formatError = require('./formatError')
 
-const repositories = exportFolder(process.cwd(), '-repository', { basepath: isProduction ? 'build/repositories' : 'src/repositories' })
-const services = exportFolder(process.cwd(), '-service', { basepath: isProduction ? 'build/services' : 'src/services' })
-const utils = exportFolder(process.cwd(), '-util', { 
-  basepath: isProduction ? 'build/utils' : 'src/utils',
+const basePath = path.join(process.cwd(), isProduction ? 'build' : 'src')
+
+const repositories = exportFolder(path.join(basePath, 'repositories'), '-repository', { mode })
+const services = exportFolder(path.join(basePath, 'services'), '-service', { mode })
+const utils = exportFolder(path.join(basePath, 'utils'), '-util', { 
   inject: {
     repositories,
     services
-  }
+  },
+  mode
 })
 
-const { CODES } = require(path.join(process.cwd(), isProduction ? 'build/errors' : 'src/errors'))
+const { CODES } = require(path.join(basePath, 'errors'))
 
 exports.repositories = repositories
 exports.services = services
